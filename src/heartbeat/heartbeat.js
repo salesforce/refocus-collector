5b9bcf4be7bc77ac6fd5b52da99c79567a60e0ab
jgraff <jgraff@salesforce.com>
2017-05-27 14:23:17 -0700

    send heartbeat

---
+++ src/heartbeat/heartbeat.js
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2017, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or
+ * https://opensource.org/licenses/BSD-3-Clause
+ */
+
+/**
+ * src/heartbeat.js
+ */
+'use strict';
+const debug = require('debug')('refocus-collector:heartbeat');
+const errors = require('../errors/errors');
+const request = require('superagent');
+const config = require('../config/config');
+
+/**
+ * Send a heartbeat to the Refocus server
+ * @returns {Request} - the request sent to the Refocus server
+ * @throws {ValidationError} - if required config fields are missing
+ */
+function sendHeartbeat() {
+  debug('Entered sendHeartbeat');
+  let collectorName, baseUrl, token, path, url;
+
+  try {
+    //assume the registry only has one entry for this version
+    collectorName = Object.keys(config.registry)[0];
+    //collectorName = config.collectorName;
+    baseUrl = config.registry[collectorName].url;
+    token = config.registry[collectorName].token;
+    path = `/v1/collectors/${collectorName}/heartbeat`;
+    url = baseUrl + path;
+
+    if (baseUrl == null) {
+      throw new errors.ValidationError(`No url in registry for ${collectorName}`);
+    }
+
+    const body = {
+      logLines: [],
+    };
+
+    debug(`sendHeartbeat sending request. url: ${url} body: %o`, body);
+
+    return request.post(url)
+    .set('Authorization', token)
+    .send(body);
+
+    //don't actually send it in this version
+    //.end((err, res) => {
+    //  if (err) {
+    //    debug('sendHeartbeat failed');
+    //    throw err;
+    //  }
+    //debug('sendHeartbeat successful');
+    //});
+  }
+  catch (err) {
+    if (config == null || config.registry == null) {
+      throw new errors.ValidationError('Registry config is missing');
+    } else if (config.registry[collectorName] == null) {
+      throw new errors.ValidationError(`Registry entry empty for ${collectorName}`);
+    } else {
+      throw err;
+    }
+  }
+
+}
+
+module.exports = {
+  sendHeartbeat,
+  config,  // export for testing
+};
