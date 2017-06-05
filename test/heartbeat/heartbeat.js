7156ad4a917d1ada10c69b7b4c6698599e5ab498
jgraff <jgraff@salesforce.com>
2017-06-05 11:48:48 -0700

    send heartbeat - registry change

--- test/heartbeat/heartbeat.js
+++ test/heartbeat/heartbeat.js
@@ -10,7 +10,7 @@
  * tests/heartbeat/heartbeat.js
  */
 'use strict';
-require('../../src/config/config').setRegistry({ registry: {} });
+require('../../src/config/config').setRegistry({});
 const config = require('../../src/config/config').getConfig();
 const expect = require('chai').expect;
 const heartbeat = require('../../src/heartbeat/heartbeat');
