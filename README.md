21a8aa9f893bd8519b77c094cedc9eb1988f50b5
Anny He <anny.he@salesforce.com>
2017-06-13 08:04:23 -0700

    initial commit to make package into executable

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # refocus-collector
+
+  Usage: rc-start [options] [command]
+
+
+  Commands:
+
+    register <name> <url> <token>  Register collector by name, refocus url and API token
+    start <name>                   Start given collector
+    stop <name>                    Stop given collector
+    status <name>                  Show status of collector
+    deregister <name>              Deregister given collector
+    help [cmd]                     display help for [cmd]
+
+  Options:
+
+    -h, --help     output usage information
+    -V, --version  output the version number
+
+  Examples:
+
+    $ rc --help
+    $ rc register test test@test.com eygduyguygijfdhkfjhkfdhg
+    $ rc start test
+    $ rc stop test
+    $ rc status test
+    $ rc deregister test
+  ____       __                         ____      _ _           _
+ |  _ \ ___ / _| ___   ___ _   _ ___   / ___|___ | | | ___  ___| |_ ___  _ __
+ | |_) / _ \ |_ / _ \ / __| | | / __| | |   / _ \| | |/ _ \/ __| __/ _ \| '__|
+ |  _ <  __/  _| (_) | (__| |_| \__ \ | |__| (_) | | |  __/ (__| || (_) | |
+ |_| \_\___|_|  \___/ \___|\__,_|___/  \____\___/|_|_|\___|\___|\__\___/|_|
--- index.js
+++ index.js
@@ -21,6 +21,12 @@ const constants = require('./src/constants');
 const cmdStart = require('./src/commands/start');
 const conf = require('./src/config/config');
 
+// for package commands
+const program = require('commander');
+
+// for the large refocus font in command line
+const figlet = require('figlet');
+
 try {
   conf.setRegistry(constants.registryLocation);
   cmdStart.execute();
@@ -28,3 +34,30 @@ try {
   logger.error(err.message);
   logger.error(err);
 }
+
+program
+  .version('0.0.1')
+  .command('register <name> <url> <token>', 'Register collector by name, refocus url and API token')
+  .command('start <name>', 'Start given collector')
+  .command('stop <name>', 'Stop given collector')
+  .command('status <name>', 'Show status of collector')
+  .command('deregister <name>', 'Deregister given collector')
+
+program.on('--help', function() {
+  console.log('  Examples:');
+  console.log('');
+  console.log('    $ rc --help');
+  console.log('    $ rc register test test@test.com eygduyguygijfdhkfjhkfdhg');
+  console.log('    $ rc start test');
+  console.log('    $ rc stop test');
+  console.log('    $ rc status test');
+  console.log('    $ rc deregister test');
+  console.log(figlet.textSync('Refocus Collector'));
+
+});
+
+program.parse(process.argv);
+
+module.exports = {
+  program
+};
