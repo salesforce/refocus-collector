2caf64f21927c8e84c7d39a4e6948d5f52f2e651
Ian Goldstein <igoldstein@salesforce.com>
2017-07-03 10:59:14 -0700

    update the safeEval fn to allow try/catch and Math and RegExp but still not allow module.require and eval etc.

--- src/utils/evalUtils.js
+++ src/utils/evalUtils.js
@@ -11,7 +11,6 @@
  */
 'use strict';
 const debug = require('debug')('refocus-collector:evalUtils');
-const notevil = require('notevil');
 const errors = require('../errors/errors');
 const evalValidation = require('./evalValidation');
 const ERROR_MESSAGE = {
@@ -30,93 +29,28 @@ const SAMPLE_BODY_MAX_LEN = 4096;
 
 /*
  * This makes it possible for the transform function to refer to ctx and res
- * and subject and subjects and JSON directly, without having to refer to them
- * as attribtues of args.
+ * and subject and subjects directly, without having to refer to them as
+ * attribtues of args.
  */
-const transformFnPrefix = 'const JSON = args._JSON; ' +
-  'const ctx = args.ctx; ' +
+const transformFnPrefix = 'const ctx = args.ctx; ' +
   'const res = args.res; ' +
   'const subject = args.subject; ' +
   'const subjects = args.subjects; ' +
-  'const SAMPLE_BODY_MAX_LEN = args._SAMPLE_BODY_MAX_LEN; ' +
-  'const Math = args._Math;';
+  'const SAMPLE_BODY_MAX_LEN = args._SAMPLE_BODY_MAX_LEN; ';
 
 /*
  * This makes it possible for the toUrl function to refer to ctx and subject
- * and subjects and JSON directly, without having to refer to them as
- * attribtues of args.
+ * and subjects directly, without having to refer to them as attribtues of
+ * args.
  */
-const toUrlFnPrefix = 'const JSON = args._JSON; ' +
-  'const ctx = args.ctx; ' +
+const toUrlFnPrefix = 'const ctx = args.ctx; ' +
   'const subject = args.subject; ' +
   'const subjects = args.subjects; ';
 
 /**
- * Modifies the "args" object. Gives the function body access to JSON.parse
- * and JSON.stringify by adding them to the args. The defn of these functions
- * comes from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON#Polyfill.
- *
- * @param {Object} args - The arguments to pass to the function
- */
-function addJson(args) {
-  args._JSON = {
-    parse: (s) => eval(`(${s})`),
-    stringify: (function () {
-      var toString = Object.prototype.toString;
-      var isArray = Array.isArray || function (a) {
-        return toString.call(a) === '[object Array]';
-      };
-
-      var escMap = {
-        '"': '\\"',
-        '\\': '\\\\',
-        '\b': '\\b',
-        '\f': '\\f',
-        '\n': '\\n',
-        '\r': '\\r',
-        '\t': '\\t',
-      };
-      var escFunc = (m) => escMap[m] || '\\u' +
-        (m.charCodeAt(0) + 0x10000).toString(16).substr(1);
-      var escRE = /[\\"\u0000-\u001F\u2028\u2029]/g;
-      return function stringify(value) {
-        if (value == null) {
-          return 'null';
-        } else if (typeof value === 'number') {
-          return isFinite(value) ? value.toString() : 'null';
-        } else if (typeof value === 'boolean') {
-          return value.toString();
-        } else if (typeof value === 'object') {
-          if (typeof value.toJSON === 'function') {
-            return stringify(value.toJSON());
-          } else if (isArray(value)) {
-            var res = '[';
-            for (let i = 0; i < value.length; i++)
-              res += (i ? ', ' : '') + stringify(value[i]);
-            return res + ']';
-          } else if (toString.call(value) === '[object Object]') {
-            var tmp = [];
-            for (const k in value) {
-              if (value.hasOwnProperty(k)) {
-                tmp.push(stringify(k) + ': ' + stringify(value[k]));
-              }
-            }
-
-            return `{${tmp.join(', ')}}`;
-          }
-        }
-
-        return `"${value.toString().replace(escRE, escFunc)}"`;
-      };
-    })(),
-  };
-} // addMoreArgs
-
-/**
  * Safely execute the transform or toUrl code from the sample generator
  * template, blocking certain global functions and node functions, and
- * returning the result of the eval (in the case of transform, an array of
- * samples; in the case of toUrl, a url string). No access to globals.
+ * returning the result of the eval. No access to globals.
  *
  * @param {String} functionBody - The body of the function to execute.
  * @param {Object} args - Args to pass through to the function.
@@ -125,21 +59,35 @@ function addJson(args) {
  */
 function safeEval(functionBody, args) {
   'use strict';
+  if (!args) {
+    args = {};
+  }
+
+  /*
+   * Make sure user-provided function can't mess with console, process or eval
+   * but *can* use JSON object.
+   */
+  args._JSON = JSON;
+  functionBody = 'const console = undefined; ' +
+    'const process = undefined; ' +
+    'const eval = undefined; ' +
+    'const JSON = args._JSON; ' +
+    functionBody;
   try {
     /*
      * TODO generate the fn only once upon receiving the generator, store
      *  the generated func as part of the generator config in the config
      */
-    const func = notevil.Function('args', functionBody);
-    debug(`evalUtils.safeEval generated function: ${func}`);
-    debug(`evalUtils.safeEval calling function with args: ${args}`);
+    const func = new Function(`return function(args) { ${functionBody} }`)();
+    debug(`evalUtils.newSafeEval generated function: ${func}`);
+    debug(`evalUtils.newSafeEval calling function with args: ${args}`);
     const retval = func(args);
     debug(`evalUtils.safeEval returning: ${retval}`);
     return retval;
   } catch (err) {
     throw new errors.FunctionBodyError(`${err.name}: ${err.message}`);
   }
-} // safeEval
+}
 
 /**
  * Makes sure that the args contain all the expected attributes.
@@ -207,9 +155,7 @@ function validateToUrlArgs(args) {
 function safeTransform(functionBody, args) {
   debug('Entered evalUtils.safeTransform');
   validateTransformArgs(args);
-  addJson(args);
   args._SAMPLE_BODY_MAX_LEN = SAMPLE_BODY_MAX_LEN;
-  args._Math = Math;
   const retval = safeEval(transformFnPrefix + functionBody, args);
   if (!Array.isArray(retval)) {
     throw new errors.TransformError(ERROR_MESSAGE.TRANSFORM.NOT_ARRAY);
@@ -250,7 +196,6 @@ function safeTransform(functionBody, args) {
 function safeToUrl(functionBody, args) {
   debug('Entered evalUtils.safeToUrl');
   validateToUrlArgs(args);
-  addJson(args);
   const retval = safeEval(toUrlFnPrefix + functionBody, args);
   if (typeof retval !== 'string') {
     throw new errors.ToUrlError(ERROR_MESSAGE.TO_URL.NOT_STRING);
