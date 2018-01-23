7913903650f70a8018bd590f0b4a436c8345a0ed
Shriram <sshankar@salesforce.com>
2018-01-23 14:30:05 -0800

    pause: added pause command

---
+++ src/commands/refocus-collector-pause.js
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or
+ * https://opensource.org/licenses/BSD-3-Clause
+ */
+
+/**
+ * src/commands/refocus-collector-pause.js
+ *
+ * Executes the "pause" command.
+ */
+'use strict'; // eslint-disable-line strict
+const program = require('commander');
+const logger = require('winston');
+const cmdUtils = require('./utils');
+const doPost = require('../utils/httpUtils.js').doPostToRefocus;
+
+program
+  .option('-n, --collectorName <collectorName>', 'The name of the ' +
+    'collector to be paused')
+  .option('-u, --refocusUrl <refocusUrl>', 'The url of the refocus ' +
+    'instance this collector will send to')
+  .option('-t, --accessToken <accessToken>', 'A valid refocus token')
+  .option('-r, --refocusProxy <refocusProxy>', 'Proxy to Refocus')
+  .parse(process.argv);
+
+if (!cmdUtils.validateArgs(program)) {
+  process.exit(1);
+}
+
+const config = cmdUtils.setupConfig(program);
+const pausePath = `/v1/collectors/${config.name}/pause`;
+
+logger.log('Pause =>', config.name, config.refocus.url + pausePath);
+
+// Request to Refocus to pause the collector
+doPost(pausePath)
+.then(() => {
+  logger.info(`Request to pause ${config.name} has been accepted.`);
+})
+.catch((err) => {
+  logger.error(err.message);
+  logger.error(err.explanation);
+  logger.error(err.response);
+});
---
+++ test/commands/pause.js
@@ -0,0 +1,140 @@
+/**
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or
+ * https://opensource.org/licenses/BSD-3-Clause
+ */
+
+/**
+ * test/commands/pause.js
+ */
+'use strict'; // eslint-disable-line strict
+const expect = require('chai').expect;
+const fork = require('child_process').fork;
+
+describe('test/commands/pause >', () => {
+  const collectorName = 'collector1';
+  const refocusUrl = 'http://www.refocus-pause-test.com';
+  const accessToken = 'abcdefghijklmnopqrstuvwxyz';
+  const refocusProxy = 'http://abcproxy.com';
+  const missingCollectorNameError = 'error: You must specify a ' +
+    'collector name.\n';
+  const missingUrlError = 'error: You must specify the url of the ' +
+    'refocus instance.\n';
+  const missingTokenError = 'error: You must specify an access token.\n';
+
+  it('ok', (done) => {
+    const args = [
+      '--collectorName', collectorName, '--refocusUrl', refocusUrl,
+      '--accessToken', accessToken, '--refocusProxy', refocusProxy,
+    ];
+    const opts = { silent: true };
+    const pause = fork('src/commands/refocus-collector-pause.js',
+      args, opts);
+    pause.on('close', (code) => {
+      expect(code).to.equal(0);
+      done();
+    });
+  });
+
+  it('no collectorName', (done) => {
+    const args = [
+      '--refocusUrl', refocusUrl, '--accessToken', accessToken,
+    ];
+    const opts = { silent: true };
+    const pause = fork('src/commands/refocus-collector-pause.js',
+      args, opts);
+    pause.stderr.on('data', (data) => {
+      expect(data.toString()).to.equal(missingCollectorNameError);
+    });
+    pause.on('close', (code) => {
+      expect(code).to.equal(1);
+      done();
+    });
+  });
+
+  it('no refocusUrl', (done) => {
+    const args = [
+      '--collectorName', collectorName, '--accessToken', accessToken,
+    ];
+    const opts = { silent: true };
+    const pause = fork('src/commands/refocus-collector-pause.js',
+      args, opts);
+    pause.stderr.on('data', (data) => {
+      expect(data.toString()).to.equal(missingUrlError);
+    });
+    pause.on('close', (code) => {
+      expect(code).to.equal(1);
+      done();
+    });
+  });
+
+  it('no accessToken', (done) => {
+    const args = [
+      '--collectorName', collectorName, '--refocusUrl', refocusUrl,
+    ];
+    const opts = { silent: true };
+    const pause = fork('src/commands/refocus-collector-pause.js',
+      args, opts);
+    pause.stderr.on('data', (data) => {
+      expect(data.toString()).to.equal(missingTokenError);
+    });
+    pause.on('close', (code) => {
+      expect(code).to.equal(1);
+      done();
+    });
+  });
+
+  it('ok, no refocusProxy', (done) => {
+    const args = [
+      '--collectorName', collectorName, '--refocusUrl', refocusUrl,
+      '--accessToken', accessToken,
+    ];
+    const opts = { silent: true };
+    const pause = fork('src/commands/refocus-collector-pause.js',
+      args, opts);
+    pause.on('close', (code) => {
+      expect(code).to.equal(0);
+      done();
+    });
+  });
+
+  it('use environment variables if not specifed', (done) => {
+    const args = [];
+    const opts = {
+      silent: true,
+      env: {
+        RC_COLLECTOR_NAME: collectorName,
+        RC_REFOCUS_URL: refocusUrl,
+        RC_ACCESS_TOKEN: accessToken,
+        RC_REFOCUS_PROXY: refocusProxy,
+      },
+    };
+    const pause = fork('src/commands/refocus-collector-pause.js',
+      args, opts);
+    pause.on('close', (code) => {
+      expect(code).to.equal(0);
+      done();
+    });
+  });
+
+  it('mix of arguments and environment variables', (done) => {
+    const args = ['--collectorName', collectorName];
+    const opts = {
+      silent: true,
+      env: {
+        RC_REFOCUS_URL: refocusUrl,
+      },
+    };
+    const pause = fork('src/commands/refocus-collector-pause.js',
+      args, opts);
+    pause.stderr.on('data', (data) => {
+      expect(data.toString()).to.equal(missingTokenError);
+    });
+    pause.on('close', (code) => {
+      expect(code).to.equal(1);
+      done();
+    });
+  });
+});
