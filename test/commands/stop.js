48ed65ccaa96379f3832a55e0427fd3ef5d89a3a
Shriram <sshankar@salesforce.com>
2018-01-23 14:27:26 -0800

    stop: refactored and cleaned up stop command

--- test/commands/stop.js
+++ test/commands/stop.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2017, salesforce.com, inc.
+ * Copyright (c) 2018, salesforce.com, inc.
  * All rights reserved.
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or
@@ -9,200 +9,132 @@
 /**
  * test/commands/stop.js
  */
-'use strict';
+'use strict'; // eslint-disable-line strict
 const expect = require('chai').expect;
-const start = require('../../src/commands/start');
-const stop = require('../../src/commands/stop');
-const repeater = require('../../src/repeater/repeater');
-const listener = require('../../src/heartbeat/listener');
-const nock = require('nock');
-const sinon = require('sinon');
-const request = require('superagent');
-require('superagent-proxy')(request);
-const collectorName = 'collector1';
-const refocusUrl = 'http://www.refocusserver.com';
-const accessToken = 'abcdefghijklmnopqrstuvwxyz';
-const collectorToken = 'zyxwvutsrqponmlkjihgfedcba';
-const refocusProxy = 'http://refocusproxy.com';
-const dataSourceProxy = 'http://datasourceproxy.com';
-const httpStatus = require('../../src/constants.js').httpStatus;
-const queueUtils = require('../../src/utils/queueUtils');
-
-beforeEach((done) => {
-  nock(refocusUrl, {
-    reqheaders: { authorization: accessToken },
-  })
-  .post('/v1/collectors/start',
-    { name: `${collectorName}`, version: '1.0.0' })
-  .reply(httpStatus.CREATED, { token: collectorToken });
-
-  nock(refocusUrl, {
-    reqheaders: { authorization: accessToken },
-  })
-  .post(`/v1/${collectorName}/stop`, { name: `${collectorName}` })
-  .reply(httpStatus.OK);
-  done();
-});
+const fork = require('child_process').fork;
 
 describe('test/commands/stop >', () => {
-  // TODO: child process fails on travis with error
-  // /bin/sh: 1: refocus-collector: not found
-  it('not ok, cannot run stop without running start first', (done) => {
-    const { exec } = require('child_process');
-    exec('refocus-collector stop',
-    (error, stdout, stderr) => {
-      console.log('error--', error);
-      if (error) {
-        console.error(`exec error: ${error}`);
-        return done();
-      }
+  const collectorName = 'collector1';
+  const refocusUrl = 'http://www.refocus-stop-test.com';
+  const accessToken = 'abcdefghijklmnopqrstuvwxyz';
+  const refocusProxy = 'http://abcproxy.com';
+  const missingCollectorNameError = 'error: You must specify a ' +
+    'collector name.\n';
+  const missingUrlError = 'error: You must specify the url of the ' +
+    'refocus instance.\n';
+  const missingTokenError = 'error: You must specify an access token.\n';
 
-      return done('Expected Stop to fail');
+  it('ok', (done) => {
+    const args = [
+      '--collectorName', collectorName, '--refocusUrl', refocusUrl,
+      '--accessToken', accessToken, '--refocusProxy', refocusProxy,
+    ];
+    const opts = { silent: true };
+    const stop = fork('src/commands/refocus-collector-stop.js',
+      args, opts);
+    stop.on('close', (code) => {
+      expect(code).to.equal(0);
+      done();
     });
   });
 
-  it('stop without proxy', (done) => {
-    const spy = sinon.spy(queueUtils, 'flushAllBufferedQueues');
-    start.execute(collectorName, refocusUrl, accessToken, {})
-    .then(() => {
-      // make sure start created the repeat
-      expect(repeater.tracker.heartbeat).to.be.an('object');
-    })
-    .then(() => stop.execute())
-    .then((res) => {
-      expect(res.status).to.equal(httpStatus.OK);
-      expect(repeater.tracker.heartbeat).to.equal(undefined);
-
-      // make sure that flushAllBufferedQueues was called
-      expect(spy.calledOnce).to.equal(true);
-      spy.restore();
+  it('no collectorName', (done) => {
+    const args = [
+      '--refocusUrl', refocusUrl, '--accessToken', accessToken,
+    ];
+    const opts = { silent: true };
+    const stop = fork('src/commands/refocus-collector-stop.js',
+      args, opts);
+    stop.stderr.on('data', (data) => {
+      expect(data.toString()).to.equal(missingCollectorNameError);
+    });
+    stop.on('close', (code) => {
+      expect(code).to.equal(1);
       done();
-    })
-    .catch((err) => done(err));
-  });
-
-  it('stop with proxy', (done) => {
-    const spy = sinon.spy(queueUtils, 'flushAllBufferedQueues');
-    start.execute(collectorName, refocusUrl, accessToken,
-      { refocusProxy, dataSourceProxy }
-    )
-    .then(() => {
-      // make sure start created the repeat
-      expect(repeater.tracker.heartbeat).to.be.an('object');
-    })
-    .then(() => stop.execute())
-    .then((res) => {
-      expect(res.status).to.equal(httpStatus.OK);
-      expect(repeater.tracker.heartbeat).to.equal(undefined);
-
-      // make sure that flushAllBufferedQueues was called
-      expect(spy.calledOnce).to.equal(true);
-      spy.restore();
-      done();
-    })
-    .catch((err) => done(err));
-  });
-
-  it('force stop, flush should not be called', (done) => {
-    const spy = sinon.spy(queueUtils, 'flushAllBufferedQueues');
-    start.execute(collectorName, refocusUrl, accessToken,
-      { refocusProxy, dataSourceProxy }
-    )
-    .then(() => {
-      // make sure start created the repeat
-      expect(repeater.tracker.heartbeat).to.be.an('object');
-    })
-    .then(() => stop.execute(true))
-    .then((res) => {
-      expect(res.status).to.equal(httpStatus.OK);
-      expect(repeater.tracker.heartbeat).to.equal(undefined);
-
-      // make sure that "flushAllBufferedQueues" was not called
-      expect(spy.calledOnce).to.equal(false);
-      spy.restore();
-      done();
-    })
-    .catch((err) => done(err));
-  });
-
-  it('ok, refocus proxy used in request if provided', (done) => {
-    let spy;
-    start.execute(
-      collectorName, refocusUrl, accessToken,
-      { refocusProxy, dataSourceProxy }
-    )
-    .then(() => {
-      spy = sinon.spy(request, 'post');
-      stop.execute();
-    })
-    .then(() => {
-      expect(spy.returnValues[0]._proxyUri).to.be.equal(refocusProxy);
-      spy.restore();
-      done();
-    })
-    .catch((err) => {
-      spy.restore();
-      done(err);
     });
   });
 
-  it('stop collector, with multiple generators running', (done) => {
-    const res = {
-      collectorConfig: {
-        heartbeatInterval: 50,
-        maxSamplesPerBulkRequest: 10,
+  it('no refocusUrl', (done) => {
+    const args = [
+      '--collectorName', collectorName, '--accessToken', accessToken,
+    ];
+    const opts = { silent: true };
+    const stop = fork('src/commands/refocus-collector-stop.js',
+      args, opts);
+    stop.stderr.on('data', (data) => {
+      expect(data.toString()).to.equal(missingUrlError);
+    });
+    stop.on('close', (code) => {
+      expect(code).to.equal(1);
+      done();
+    });
+  });
+
+  it('no accessToken', (done) => {
+    const args = [
+      '--collectorName', collectorName, '--refocusUrl', refocusUrl,
+    ];
+    const opts = { silent: true };
+    const stop = fork('src/commands/refocus-collector-stop.js',
+      args, opts);
+    stop.stderr.on('data', (data) => {
+      expect(data.toString()).to.equal(missingTokenError);
+    });
+    stop.on('close', (code) => {
+      expect(code).to.equal(1);
+      done();
+    });
+  });
+
+  it('ok, no refocusProxy', (done) => {
+    const args = [
+      '--collectorName', collectorName, '--refocusUrl', refocusUrl,
+      '--accessToken', accessToken,
+    ];
+    const opts = { silent: true };
+    const stop = fork('src/commands/refocus-collector-stop.js',
+      args, opts);
+    stop.on('close', (code) => {
+      expect(code).to.equal(0);
+      done();
+    });
+  });
+
+  it('use environment variables if not specifed', (done) => {
+    const args = [];
+    const opts = {
+      silent: true,
+      env: {
+        RC_COLLECTOR_NAME: collectorName,
+        RC_REFOCUS_URL: refocusUrl,
+        RC_ACCESS_TOKEN: accessToken,
+        RC_REFOCUS_PROXY: refocusProxy,
       },
-      generatorsAdded: [
-        {
-          name: 'Core_Trust1',
-          aspects: [{ name: 'Latency', timeout: '1m' }],
-          interval: 6000,
-          generatorTemplateName: 'refocus-trust1-collector',
-          generatorTemplate: {
-            name: 'abc-gen-templ',
-            connection: {
-              url: 'https://coretrust1.data',
-              bulk: true,
-            },
-          },
-          subjectQuery: 'absolutePath=Parent.Child.*&tags=Primary',
-          context: { baseUrl: 'https://coretrust1.data', },
-        },
-        {
-          name: 'Core_Funnel1',
-          aspects: [{ name: 'CrossWind', timeout: '1m' }],
-          interval: 1000,
-          context: { baseUrl: 'https://funnel.data', },
-          generatorTemplate: {
-            name: 'abc-gen-templ',
-            connection: {
-              url: 'https://funnel.data',
-              bulk: true,
-            },
-          },
-        },
-      ],
     };
-    start.execute(collectorName, refocusUrl, accessToken,
-      { refocusProxy, dataSourceProxy }
-    )
-    .then(() => {
-      listener.handleHeartbeatResponse(null, res);
-
-      // make sure start created the repeat
-      expect(repeater.tracker.heartbeat).to.be.an('object');
-      expect(repeater.tracker.Core_Funnel1).to.be.an('object');
-      expect(repeater.tracker.Core_Trust1).to.be.an('object');
-    })
-    .then(() => stop.execute())
-    .then((stopRes) => {
-      expect(stopRes.status).to.equal(httpStatus.OK);
-      expect(repeater.tracker.heartbeat).to.equal(undefined);
-      expect(repeater.tracker.Core_Funnel1).to.equal(undefined);
-      expect(repeater.tracker.Core_Trust1).to.equal(undefined);
+    const stop = fork('src/commands/refocus-collector-stop.js',
+      args, opts);
+    stop.on('close', (code) => {
+      expect(code).to.equal(0);
       done();
-    })
-    .catch((err) => done(err));
+    });
+  });
+
+  it('mix of arguments and environment variables', (done) => {
+    const args = ['--collectorName', collectorName];
+    const opts = {
+      silent: true,
+      env: {
+        RC_REFOCUS_URL: refocusUrl,
+      },
+    };
+    const stop = fork('src/commands/refocus-collector-stop.js',
+      args, opts);
+    stop.stderr.on('data', (data) => {
+      expect(data.toString()).to.equal(missingTokenError);
+    });
+    stop.on('close', (code) => {
+      expect(code).to.equal(1);
+      done();
+    });
   });
 });
-
