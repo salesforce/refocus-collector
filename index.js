5349124eef6ee89ea9e4d449ed0c5188ead7c12d
jgraff <jgraff@salesforce.com>
2018-10-15 15:30:10 -0700

    publish to npm for testing

---
+++ index.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+/**
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or
+ * https://opensource.org/licenses/BSD-3-Clause
+ */
+
+/**
+ * index.js
+ *
+ * Main module for the refocus-collector.
+ */
+'use strict'; // eslint-disable-line strict
+
+// allow the commands to be run directly for testing
+module.exports = {
+  start: () => require('./src/commands/refocus-collector-start'),
+  stop: () => require('./src/commands/refocus-collector-stop'),
+  pause: () => require('./src/commands/refocus-collector-pause'),
+  resume: () => require('./src/commands/refocus-collector-resume'),
+  status: () => require('./src/commands/refocus-collector-status'),
+  reregister: () => require('./src/commands/refocus-collector-reregister'),
+  deregister: () => require('./src/commands/refocus-collector-deregister'),
+};
